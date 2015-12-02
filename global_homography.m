2a457feaa0e8bcf59adc01cad22ed306eac09cea
Giyoung <giyoung@unist.ac.kr>
2015-12-02 15:23:44 +0900

    estimate global homography

---
+++ global_homography.m
@@ -0,0 +1,30 @@
+function [H] = global_homography(f1, f2)
+% Estimate the global homograpohy
+% 
+% The function to calculate the global homography of target image (I2)
+% to the reference image (I1). Accoding to the matching specific points
+% (f1,f2), this function calculates the estimation of the homography.
+% 
+% Input Parameters:
+%               f1, f2:     matching specific points of each images. (2XN)
+% 
+% Returns:
+%       H:      global homography size of 3X3
+% 
+% Usage: H = global_homography(f1,f2)
+% 
+% Written by Giyoung Jeon
+% Statistical Artificial Intelligence Lab at UNIST
+% v1.0 Nov., 31st, 2015
+
+    O = [0 0 0]';
+    x_ = f1(1,:); y_ = f1(2,:); x = f2(1,:); y = f2(2,:);
+    p = [x; y; ones(size(x))];
+    A = zeros(3*size(x,2), 9);
+    for idx=1:size(x,2)
+        A(2*idx-1,:) = [p(:,idx)' O' x(idx)*p(:,idx)'];
+        A(2*idx,:) = [O' -p(:,idx)' y_(idx)*p(:,idx)'];
+    end
+    [~, ~, v] = svd(A);
+    H = reshape(v(:,end),[3 3]);
+end
